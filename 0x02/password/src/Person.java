87b4cc5c3523d4e614815108b9a9e82183332ed7
evyfarias <evelyn.fsilva@sptech.school>
2025-10-30 23:51:24 +0000

    add

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Person {
+
+    public boolean checkUser(String username) {
+        if (username == null || username.length() < 8)
+            return false;
+
+        return username.matches("^[A-Za-z0-9]+$");
+    }
+
+    public boolean checkPassword(String password) {
+        if (password == null || password.length() < 8)
+            return false;
+
+        boolean hasUpper = password.matches(".*[A-Z].*");
+        boolean hasDigit = password.matches(".*[0-9].*");
+        boolean hasSpecial = password.matches(".*[^A-Za-z0-9].*");
+
+        return hasUpper && hasDigit && hasSpecial;
+    }
+}
